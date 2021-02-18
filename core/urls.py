0f15b4ca9fb2bed153d2a940706ed9b9af300a61
caprioara <dragos.neatax@gmail.com>
2021-02-18 10:54:35 +0200

    added admin honey pot package

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('secret/', admin.site.urls),
     path('account/', include('accounts.urls', namespace='accounts')),
     path('account/', include('django.contrib.auth.urls')),
     path('', include('blog.urls', namespace='blog')),
